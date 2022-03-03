6f95843027f8ad9e92eb54e2eae2570958e5da64
Dmitry Anoschenkov <danoschenkov@gmail.com>
2022-03-03 12:08:19 +0300

    Task add completed

--- Seminar_8/data.py
+++ Seminar_8/data.py
@@ -1,13 +1,18 @@
 # Хранит информацию
 
+from ast import Return
+
+
 phone_numbers = {'Person_1': '111111',
                  'Person_2': '22222',
                  'Person_3': '333333',
 }
 
+
 def add_data(string_1, string_2):
     global phone_numbers
     phone_numbers[string_1] = string_2
+    return string_1, string_2
     
 def view_data(dictionary):
     global phone_numbers
@@ -20,17 +25,19 @@ def remove_data(string_1):
     
 def find_contact(string_1):
     global phone_numbers
-    print(f'{string_1}: {phone_numbers[string_1]}')
+    user_contact = string_1 + ": " + phone_numbers[string_1]
+    # print(user_contact)
+    return user_contact
     
-view_data(phone_numbers)
+# view_data(phone_numbers)
 
-add_data('Person_4', '444444')
+# add_data('Person_4', '444444')
 
-view_data(phone_numbers)
+# view_data(phone_numbers)
 
-remove_data('Person_1')
+# remove_data('Person_1')
 
-view_data(phone_numbers)
+# view_data(phone_numbers)
 
 find_contact('Person_2')    
 
